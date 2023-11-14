7b0d916c43a3c063602a1fff432173449aa96180
pooranjoyb <pooranjoyb2016@gmail.com>
2023-11-14 23:17:22 +0530

    Added page 2 & added navbar toggle js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// Fixing BS Scroll Navbar Glitch 
+document.addEventListener("DOMContentLoaded", function () {
+    var myOffcanvas = document.getElementById('offcanvasExample');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    document.getElementById("btn").addEventListener('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        bsOffcanvas.toggle();
+    });
+});
